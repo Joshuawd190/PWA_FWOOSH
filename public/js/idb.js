0371c2895cda7761c658e20b4c4b70f790dd4b1b
Joshua D Wood <joshuawd190@gmail.com>
2021-08-28 16:27:19 -0500

    upload transaction functionality finished

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,3 +27,71 @@ function saveRecord(record) {
 
   transactionObjectStore.add(record);
 }
+
+function uploadTransaction() {
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+  const transactionObjectStore = transaction.objectStore('new_transaction');
+
+  const getAll = transactionObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if ((getAll.result.length = 1)) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+          const pizzaObjectStore = transaction.objectStore('new_transaction');
+
+          pizzaObjectStore.clear();
+
+          alert('Saved transaction has been submitted!');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    if (getAll.result.length > 1) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+          const pizzaObjectStore = transaction.objectStore('new_transaction');
+
+          pizzaObjectStore.clear();
+
+          alert('Saved transactions have been submitted!');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener('online', uploadTransaction);
